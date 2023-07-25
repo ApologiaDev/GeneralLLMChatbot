5b14af027764071c3e2353ff907af7792c6f1c27
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-07-25 00:00:16 -0400

    loading llm and embeddings

---
+++ learn_context_index.py
@@ -0,0 +1,61 @@
+
+import os
+import json
+from argparse import ArgumentParser
+
+from dotenv import load_dotenv
+from langchain import HuggingFaceHub
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+
+# load environment variables from .env, tokens
+load_dotenv()
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAIKEY')
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = os.getenv('HUGGINGFACEHUB_API_TOKEN')
+
+
+def get_argparser():
+    argparser = ArgumentParser(description='Learn the context.')
+    argparser.add_argument('corpusdir', help='directory of the PDF books')
+    argparser.add_argument('learnconfigpath', help='path of the JSON file with the configs')
+    argparser.add_argument('outputdir', help='target directory')
+    return argparser
+
+
+def get_llm_model(config):
+    llm_config = config['llm']
+    hub = llm_config.get('hub', 'openai')
+    if hub == 'openai':
+        model = llm_config.get('model', 'gpt-3.5-turbo')
+        temperature = config.get('temperature', 0.7)
+        max_tokens = config.get('max_tokens', 600)
+        return ChatOpenAI(temperature=temperature, model_name=model, max_tokens=max_tokens)
+    elif hub == 'huggingface':
+        model = llm_config.get('model', 'google/flan-t5-xxl')
+        model_kwargs = llm_config.get('model_kwargs')  # example: {'temperature': temperature, 'max_length': max_tokens}
+        return HuggingFaceHub(repo_id=model, model_kwargs=model_kwargs)
+    else:
+        raise ValueError('Unknown LLM specified!')
+
+
+def get_embedding_model(config):
+    embedding_config = config['embedding']
+    hub = embedding_config.get('hub')
+    if hub is None:
+        return OpenAIEmbeddings()
+    elif hub == 'huggingface':
+        model = embedding_config.get('model', 'sentence-transformers/all-MiniLM-L6-v2')
+        model_kwargs = embedding_config.get('model_kwargs')
+        return HuggingFaceEmbeddings(model_name=model, model_kwargs=model_kwargs)
+
+
+if __name__ == '__main__':
+    args = get_argparser().parse_args()
+    config = json.load(open(args.learnconfigpath, 'r'))
+    if not os.path.isdir(args.outputdir):
+        raise FileNotFoundError('Output directory {} does not exist.'.format(args.outputdir))
+
+    llm = get_llm_model(config)
+    embedding = get_embedding_model(config)
